5e09509018d60a96519289b0cd17232764aaa1e7
Tanya <duka.tanik@gmail.com>
2022-07-25 22:05:37 +0300

    fix

--- src/components/UserMenu/UserMenu.js
+++ src/components/UserMenu/UserMenu.js
@@ -3,7 +3,6 @@ import { authSelectors, authOperations } from '../../redux/auth';
 
 import s from './UserMenu.module.css';
 
-import avatar from './avatar.png';
 import avatar1 from './image/user_female-512.png';
 
 export default function UserMenu() {
